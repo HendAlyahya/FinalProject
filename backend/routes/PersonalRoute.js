95f4a87af0bc4086dec241d35e0929719d3a0c53
unknown <hendAlyahya>
2021-12-13 15:15:12 +0300

    complate,

--- backend/routes/PersonalRoute.js
+++ backend/routes/PersonalRoute.js
@@ -6,13 +6,13 @@ const mongoose = require("mongoose")
 router.use(express.json());
 
 
-router.get("/getAll",(req,res)=>{
-    Personal.find({userType:"seller"}).then((data)=>{res.json(data)})
+router.get("/getAll", (req, res) => {
+    Personal.find({ userType: "seller" }).then((data) => { res.json(data) })
 })
 
-router.get("/getPersonal/:id",async (req,res)=>{
+router.get("/getPersonal/:id", async (req, res) => {
     try {
-        const user = await  Personal.findById(req.params.id )
+        const user = await Personal.findById(req.params.id)
         res.send(user);
     } catch (e) {
         res.status(500).send()
@@ -43,10 +43,10 @@ router.get("/getPersonal/:id",async (req,res)=>{
 
 // })
 
-router.get("/getPersonal/:id",async (req,res)=>{
+router.get("/getPersonal/:id", async (req, res) => {
     try {
         const person = await Personal.findById(req.params.id)
-        await person.populate( 'proudct' );
+        await person.populate('proudct');
         res.send(person);
     } catch (e) {
         res.status(500).send()
@@ -54,48 +54,86 @@ router.get("/getPersonal/:id",async (req,res)=>{
     }
 })
 
-router.post("/addProudct", async(req,res)=>{
-Personal.findById({_id: req.body.personalId}).then(PUser=>{
-    console.log("Find"+PUser)
-    Proudct.create({
-        name:req.body.name,
-        category:req.body.category ,
-        description:req.body.description, 
-        image:req.body.image,
-        price:req.body.price}).then( proudct =>{
-    console.log(proudct)
-    Personal.findByIdAndUpdate(req.body.personalId,{$push:{proudct:proudct}}).then(async(user)=>{
-    await user.save()
-    res.send(user)
-}
-)
-}
-)})
-})
-
-router.post("/addCart", async(req,res)=>{Personal.findById({_id: req.body.personalId}).then(PUser=>{
-    console.log("Find"+PUser.cart)
-    if (PUser.cart== undefined ){
-      Cart.create({cart:{proudcts:req.body.proudct,qty:req.body.qty}}).then( cart =>{
-    console.log(cart)
-    Personal.findByIdAndUpdate(req.body.personalId,{cart:cart}).then(async(user)=>{
-    await user.save()
-    res.send(user)
-}
-    )}
-)   
-    } else{
-        Cart.updateOne({cart:{proudcts:req.body.proudct,qty:req.body.qty}}).then(async(cart)=>{
-            Personal.findByIdAndUpdate(req.body.personalId,{cart:cart}).then(async(user)=>{
+router.post("/addProudct", async (req, res) => {
+    Personal.findById({ _id: req.body.personalId }).then(PUser => {
+        console.log("Find" + PUser)
+        Proudct.create({
+            name: req.body.name,
+            category: req.body.category,
+            description: req.body.description,
+            image: req.body.image,
+            price: req.body.price
+        }).then(proudct => {
+            console.log(proudct)
+            Personal.findByIdAndUpdate(req.body.personalId, { $push: { proudct: proudct } }).then(async (user) => {
                 await user.save()
                 res.send(user)
-        })
+            }
             )
         }
-    
-    
-   
+        )
+    })
 })
+
+router.post("/addCart", async (req, res) => {
+    Personal.findById({ _id: req.body.personalId }).then(PUser => {
+        console.log("Find" + PUser)
+        if (PUser.cart == undefined) {
+            Cart.create({ cart: { proudcts: req.body.proudct, qty: req.body.qty } }).then(cart1 => {
+                console.log(cart1)
+                Personal.findByIdAndUpdate(req.body.personalId, { cart: cart1 }).then(async (user) => {
+                    await user.save()
+                    res.send(user)
+                }
+                )
+            }
+            )
+        } else {
+            console.log("hh")
+            Cart.findByIdAndUpdate(PUser.cart,{$push:{ cart: { proudcts: req.body.proudct, qty: req.body.qty } }}).then(async (cart1) => {
+                Personal.findByIdAndUpdate(req.body.personalId, { cart:cart1}).then(async (user) => {
+                    await user.save()
+                    res.send(user)
+                })
+            }
+            )
+        }
+    })
+}
+);
+
+
+
+
+
+
+router.put("/update", (req, res) => {
+    Personal.findOneAndUpdate({ _id: req.body.id },
+        {
+            Phone: req.body.Phone, Address: req.body.Address, image: req.body.image,
+            gender: req.body.gender, email: req.body.email, name: req.body.name
+        }, () => {
+            console.log('updated')
+        });
+
+
+    res.send('updated')
+});
+
+
+router.delete("/deletePersonal/:id", async (req, res) => {
+    try {
+        const personal = await Personal.findByIdAndDelete({ _id: req.params.id });
+        if (!personal) {
+            return rse.status(404).send()
+        }
+        // if all is well
+        res.send(personal)
+        // send the deleted Personal
+    } catch (e) {
+        res.status(500).send()
+        console.error(e)
+    }
 });
 
 
@@ -103,35 +141,5 @@ router.post("/addCart", async(req,res)=>{Personal.findById({_id: req.body.person
 
 
 
-router.put("/update", (req, res) => {Personal.findOneAndUpdate({ _id: req.body.id },
-{ Phone: req.body.Phone, Address: req.body.Address,image: req.body.image,
-    gender: req.body.gender ,email: req.body.email ,name: req.body.name }, () => {
-       console.log('updated')
-   }); 
- 
- 
-   res.send('updated')
- });
- 
 
-router.delete("/deletePersonal/:id",async (req,res)=>{
-     try{
-         const personal = await Personal.findByIdAndDelete({_id: req.params.id});
-    if (!personal){
-        return rse.status(404).send()
-    } 
-    // if all is well
-    res.send(personal)
-    // send the deleted Personal
-    }catch (e){
-        res.status(500).send()
-        console.error(e)}
-    });
-
-
-
-    
-
-
-
-module.exports= router;
+module.exports = router;
