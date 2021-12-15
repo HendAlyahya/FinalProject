ba95605cfe99a63e9ccce55a76906b6ea219b12a
unknown <hendAlyahya>
2021-12-15 16:47:17 +0300

    complate

---
+++ personalshopper/src/components/PersonalDitalis.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import {useEffect , useState} from "react"
+
+function PersonalDitalis() {
+    const [User,setUser]=useState()
+    const [Loading,setLoading]=useState()
+    useEffect(() => {
+        axios.get(`http://localhost:3001/Personal/getPersonal/${id}`).then((res) => {
+          console.log(res);
+          setUser(res.data);
+          setLoading(false);
+        });
+      }, []);
+
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default PersonalDitalis
