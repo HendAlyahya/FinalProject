c6537328cde2688ae231fe7d4cea1c763c2c8df8
unknown <hendAlyahya>
2021-12-21 15:06:45 +0300

    complate

--- personalshopper/src/components/PrivacyPolicy.js
+++ personalshopper/src/components/PrivacyPolicy.js
@@ -2,9 +2,26 @@ import React from 'react'
 
 function PrivacyPolicy() {
     return (
-        <div className='Privacy'><hr></hr>
-            <h1>Privacy Policy:</h1>
-            <h6>When you use the Your Personal Shopper service, as part of the shipping, importing, exporting, and buying and/or selling process, we collect the personal information you give us such as your name, address and email address. When you browse our website or associated entities or DBAs, we also automatically receive your computer’s internet protocol (IP) address in order to provide us with information that helps us learn about your browser and operating system. We may send you emails about our website, market place, new products, promotions and other updates.</h6><hr></hr>
+        <div className='Privacy'>
+            <Card>
+        <Card.Header><h1>Privacy Policy:</h1></Card.Header>
+        <Card.Body>
+          <blockquote className="blockquote mb-0">
+            <p>
+              {' '}
+When you use the Your Personal Shopper service, as part of the shipping, importing, exporting, 
+and buying and/or selling process, we collect the personal information you give us such as your name, 
+address and email address. When you browse our website or associated entities or DBAs,
+ we also automatically receive your computer’s internet protocol (IP) address in order to provide us 
+ with information that helps us learn about your browser and operating system. We may send you emails about our website, 
+ market place, new products, promotions and other updates.
+
+              {' '}
+            </p>
+            
+          </blockquote>
+        </Card.Body>
+      </Card>
         </div>
     )
 }
